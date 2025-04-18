611947b821476543da7e297c766efed1d68c1e61
Stu <stumac1000@yahoo.com>
2025-04-18 15:31:44 -0700

    Week 6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 //* **`` Middleware ``**
+app.use(express.static("./public"));
 app.use(express.json());
 
 //* **`` Routes ``**
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
+//* **`` Errors  ``**
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+//* **`` Connection  ``**
 const port = 3000;
 
 const start = async () => {
