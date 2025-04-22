c7b7aa0de997c4c3c562a65f1c0ceffd4f3489d7
Stu <stumac1000@yahoo.com>
2025-04-22 13:01:17 -0700

    Create a collection in MongoDB Populate our database with data from the products.json file.

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
-// async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -14,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 //* **`` Products Route ``**
 
@@ -27,7 +32,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    //todo **`` Connect to DB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.error(error);
