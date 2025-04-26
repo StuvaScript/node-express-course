6e7f2061e9a0bd948fed9bf2e247bc930dbdc70d
stu <stumac1000@yahoo.com>
2025-04-26 10:34:19 -0700

    Implement select search

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ab";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -24,9 +21,25 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  console.log("queryObject:", queryObject);
+  // console.log("queryObject:", queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  //* **`` Sort ``**
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
