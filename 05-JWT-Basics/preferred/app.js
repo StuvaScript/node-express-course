c4cb045296f8b990b531e962bea26f7457d76c48
stu <stumac1000@yahoo.com>
2025-04-30 17:07:00 -0700

    Add error handling Installed express-async-errors package.
    Had to install an older version of Express to be
    compatable with express-async-errors.
    
    Created a 404 page not found module.
    
    Created a generic error handler module.

--- 05-JWT-Basics/preferred/app.js
+++ 05-JWT-Basics/preferred/app.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
-const logonRouter = require("./routes/logon");
 const app = express();
+const logonRouter = require("./routes/logon");
+const pageNotFound = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
 //* **`` Middleware ``**
 app.use(express.json());
 
 //* **`` Routes ``**
 app.use("/api/v1", logonRouter);
+
+//* **`` Errors ``**
+app.use(pageNotFound);
+app.use(errorMiddleware);
 
 //* **`` Connection ``**
 const port = process.env.PORT || 3000;
