6ccbe80e10b91c466f9fac8796695cfcf64c8be0
stu <stumac1000@yahoo.com>
2025-05-01 12:22:20 -0700

    Create a JSON Web token

--- 05-JWT-Basics/preferred/controllers/logon.js
+++ 05-JWT-Basics/preferred/controllers/logon.js
@@ -1,8 +1,20 @@
-const logon = async (req, res) => {
-  const { name, password } = req.body;
-  console.log({ name, password });
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
 
-  res.status(200).json({ token: "Super secret token" });
+const logon = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+
+  const id = new Date().getDate(); //* <-- Dummy data
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const hello = async (req, res) => {
